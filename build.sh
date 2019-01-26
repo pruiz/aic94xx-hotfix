8885e3bc1030fcd1ca2143b0a7f99ce9d83f8906
Pablo Ruiz <pablo.ruiz@gmail.com>
2019-01-26 03:58:03 +0100

    Added a basic build.sh script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 SWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-make -C /lib/modules/$(uname -r)/build SUBDIRS=$(SWD)/aic94xx modules
+make -C /lib/modules/$(uname -r)/build SUBDIRS=${SWD}/aic94xx modules
